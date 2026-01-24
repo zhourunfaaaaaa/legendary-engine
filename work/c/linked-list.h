df1fd49403cb93a6324d9995a13cb3d549e08b89
ineedvpn767 <qq13767708646@163.com>
2026-01-24 22:21:17 +0800

    优化后的链表

---
+++ work/c/linked-list.h
@@ -0,0 +1,9 @@
+#ifndef _LINKE_DLIST_H_
+#define _LINKE_DLIST_H_
+
+typedef struct _node {
+    int value;
+    struct _node *next;
+}Node;
+
+#endif
---
+++ work/c/linked_list.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked-list.h"
+
+typedef struct list {
+    Node *head;
+    Node *tail;
+}List;// 封装
+
+// 添加链表
+list_add(List *list,int number)
+{
+    Node *p = (Node*)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+    if (list->head == NULL) {
+        list->head = p;
+        list->tail = p;
+    } else {
+        list->tail->next = p;// 前一个链表指向新的链表
+        list->tail = p;// tail指向新的链表
+    }
+}
+
+// 遍历
+void list_print(List *list)
+{
+    Node *p;
+    for(p = list->head ;p ;p = p->next) {
+        printf("%d\t",p->value);
+    }
+    printf("\n");
+}
+
+// 删除
+void list_delete(List *list,int number)
+{
+    Node *p = list->head;
+    Node *q = NULL;
+    Node *t;
+    while (p != NULL) {
+        t = p->next;
+        if (p->value == number) {
+            if (q == NULL) {
+                list->head = t;
+            } else {
+                q->next = t;
+                free(p);
+                p = t;
+            }
+        } else {
+            q = p;
+            p = t;
+        }
+    }
+    list->tail = q;
+}
+
+// 清除整个链表
+void list_clear(List *list)
+{
+    Node *p;
+    Node *q;
+    for (p = list->head; p;p = q) {
+        q = q->next;
+        free(p);
+    }
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    List list;
+    list.head = &head;
+    int number = 0;
+    do {
+        scanf("%d",&number);
+        if (number != -1) {
+            list_add(&list,number);
+        }
+    } while (number != -1);
+    list_print(&list);
+    scanf("输入想要删除的数字：%d",&number);
+    while (number != -1) {
+        list_delete(&list,number);
+        scanf("输入想要删除的数字：%d",&number);
+    }
+    list_clear(&list);
+    
+    return 0;
+}
